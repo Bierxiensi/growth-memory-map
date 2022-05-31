f34b9d306b774f9fed3e740e597e29eb2f644d82
bierxiensi <bierxiensi@163.com>
2022-05-31 18:35:39 +0800

    🤔

---
+++ src/constant/index.ts
@@ -0,0 +1,21 @@
+interface ILngLat {
+    lng: number;
+    lat: number;
+}
+
+
+const NJ_lNG_LAT: ILngLat = {
+    lng: 118.778611,
+    lat: 32.043889,
+};
+const BJ_lNG_LAT: ILngLat = {
+    lng: 116.397428,
+    lat: 39.90923,
+};
+const Zoom11: number  = 11
+
+export {
+    NJ_lNG_LAT,
+    BJ_lNG_LAT,
+    Zoom11
+}
